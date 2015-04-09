7f7f9a1d057fcb6e9bd6bf0392e674cbe224e120
Sandeepan Kundu <sandeepan@hellogbye.com>
2015-04-09 17:57:49 -0400

    .

---
+++ MyInterfaces/IKinderJoy.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyInterfaces
+{
+    public interface IKinderJoy
+    {
+        string Greet(string name);
+        string GreetBye(string name);
+    }
+}
---
+++ NugetPackageLibraries/KinderJoy.cs
@@ -0,0 +1,22 @@
+﻿using MyInterfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NugetPackageLibraries
+{
+    public class KinderJoy : IKinderJoy
+    {
+        public string Greet(string name)
+        {
+            return string.Format("hello {0}!", name);
+        }
+
+        public string GreetBye(string name)
+        {
+            return string.Format("bye {0}!", name);
+        }
+    }
+}
